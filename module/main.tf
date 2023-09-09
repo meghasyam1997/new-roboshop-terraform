97c6ecb194bb07c8c133d1adfd75bebd5053222b
meghasyam1997 <meghasyam01@gmail.com>
2023-09-09 16:28:59 +0530

    dns domain

--- module/main.tf
+++ module/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "instances" {
+resource "aws_instance" "instance" {
   ami = data.aws_ami.ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.allow_all.id]
@@ -8,11 +8,10 @@ resource "aws_instance" "instances" {
   }
 }
 
-#resource "aws_route53_record" "record" {
-#  depends_on = [aws_instance.instances]
-#  zone_id = "Z06713411IASYL5XZHSG8"
-#  name    = "${var.component_name}-${var.env}-msdevops72.online"
-#  type    = "A"
-#  ttl     = 30
-#  records = [aws_instance.instances.private_ip]
-#}
+resource "aws_route53_record" "record" {
+  zone_id = "Z06713411IASYL5XZHSG8"
+  name    = "${var.component_name}-${var.env}.msdevops72.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance.private_ip]
+}
