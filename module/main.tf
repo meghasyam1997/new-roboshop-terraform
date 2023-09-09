ebee0f3c98493bd7f682c682646fc0772e79101a
meghasyam1997 <meghasyam01@gmail.com>
2023-09-09 16:07:12 +0530

    configure state file

--- module/main.tf
+++ module/main.tf
@@ -8,11 +8,11 @@ resource "aws_instance" "instances" {
   }
 }
 
-resource "aws_route53_record" "record" {
-  depends_on = [aws_instance.instances]
-  zone_id = "Z06713411IASYL5XZHSG8"
-  name    = "${var.component_name}-${var.env}-msdevops72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.instances.private_ip]
-}
+#resource "aws_route53_record" "record" {
+#  depends_on = [aws_instance.instances]
+#  zone_id = "Z06713411IASYL5XZHSG8"
+#  name    = "${var.component_name}-${var.env}-msdevops72.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.instances.private_ip]
+#}
