3c2ed1e26a11859ecb9f6d547f3dfafa0a758d9c
meghasyam1997 <meghasyam01@gmail.com>
2024-01-26 13:53:48 +0530

    adding tags to vpc

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 module "vpc" {
   source = "git::https://github.com/meghasyam1997/new-vpc-terraform.git"
 
-  for_each = var.vpc
+  for_each   = var.vpc
   cidr_block = each.value["cidr_block"]
+
+  tags       = local.tags
+  env        = var.env
 }
---
+++ tags.tf
@@ -0,0 +1,9 @@
+locals {
+  tags = {
+    business_unit = "Ecommerce"
+    business_type = "Retail"
+    project       = "Roboshop"
+    cost_center   = 100
+    env           = var.env
+  }
+}
